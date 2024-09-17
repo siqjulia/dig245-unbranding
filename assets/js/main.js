5faa0e125c8270d57feeb08e66a01e5f8f383238
siqjulia <jusiqueira@davidson.edu>
2024-09-17 15:25:36 -0400

    Nested img in moderation div tag

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 
 /* javascript */
 
-let moderation = document.querySelector(".moderation"); 
+let moderation = document.querySelector(".moderation");
 
-moderation.addEventListener("mouseover", function(){ 
-    this.src = "assets/img/xmoderationpolicy.png"
+moderation.addEventListener("mouseover", function () {
+    this.src = "assets/img/insanetweets.png"
 }) 
