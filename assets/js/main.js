07c0ee6fd5ec917d64ae7d50bf273e72d202a740
siqjulia <jusiqueira@davidson.edu>
2024-09-17 14:40:12 -0400

    Centering not working

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,8 @@
 
 /* javascript */
+
+let moderation = document.querySelector(".moderation"); 
+
+moderation.addEventListener("mouseover", function(){ 
+    this.src = "assets/img/xmoderationpolicy.png"
+}) 
