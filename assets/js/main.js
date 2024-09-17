ec81dd5dcb741726d6625cecec87a97e98c605e0
siqjulia <jusiqueira@davidson.edu>
2024-09-17 19:59:54 -0400

    Added console.log statement

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,11 @@
 
 /* javascript */
 
-function setNewImage()
-{ 
-  document.getElementById("modpo").src = "assets/img/insanetweets.png";
+function setNewImage() {
+    document.getElementById("modpo").src = "assets/img/insanetweets.png";
 }
 
-function setOldImage()
-{
+function setOldImage() {
     document.getElementById("modpo").src = "assets/img/xmoderationpolicy.png";
 }
+console.log("Our motivation behind this project was to call out X & Elon Musk for creating a spreading place for QAnon while claiming to have a safe and respectful environment. ");
