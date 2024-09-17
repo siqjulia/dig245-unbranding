dc85dfef1da952c780226ee2ccba2103fef0c4dd
siqjulia <jusiqueira@davidson.edu>
2024-09-17 16:38:39 -0400

    Hover not working - Khai check here

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,14 @@
 /* javascript */
 
 let moderation = document.querySelector(".moderation");
+let main = document.querySelector("main");
 
 moderation.addEventListener("mouseover", function () {
     this.src = "assets/img/insanetweets.png"
+    main.style.transition = '1s'
+    main.style.opacity = 1;
 }) 
+
+moderation.addEventListener("mouseout", function () {
+    this.src = "assets/img/xmoderationpolicy.png"
+    });
