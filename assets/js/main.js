d06f1875c6bda7ca16bfee77d09e0f0de82509c9
Khai Nguyen <khnguyen@davidson.edu>
2024-09-17 20:15:14 -0400

    Updated styles, JS, HTML, and added new images

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,11 @@ function setOldImage()
 {
     document.getElementById("modpo").src = "assets/img/xmoderationpolicy.png";
 }
+
+function setNewImageLogo() {
+  document.getElementById("modpo1").src = "assets/img/qlogo.png";
+}
+
+function setOldImageLogo() {
+  document.getElementById("modpo1").src = "assets/img/xlogo.png";
+}
