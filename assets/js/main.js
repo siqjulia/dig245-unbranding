2dfe868a64895cf7761e75ac0b9a673818918f34
siqjulia <jusiqueira@davidson.edu>
2024-09-17 22:07:47 -0400

    Added authors names to page & sources added to console

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,5 +18,5 @@ function setOldImageLogo() {
   document.getElementById("modpo1").src = "assets/img/xlogo.png";
 }
 
-console.log("Our motivation behind this project was to call out X & Elon Musk for creating a spreading place for QAnon while claiming to have a safe and respectful environment. ");
+console.log("Our motivation behind this project was to call out X & Elon Musk for creating a spreading place for QAnon while claiming to have a safe and respectful environment. Our main sources are https://www.jta.org/2023/09/21/united-states/elon-musks-x-made-an-ad-for-itself-and-showcased-a-post-pillorying-musk-for-blaming-it-all-on-the-jews and https://x.com/elonmusk/status/1767276364560773389");
 
